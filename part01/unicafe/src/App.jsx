38118f9ce9c8bb065534c9685d83e39beb814c5c
LethalSnake1337 <LethalSnake@outlook.com>
2024-09-13 07:59:45 +0200

    Exercise 1.7

--- part01/unicafe/src/App.jsx
+++ part01/unicafe/src/App.jsx
@@ -14,12 +14,19 @@ const Feedback = ({ children }) => {
 };
 
 const Statistic = ({ good, neutral, bad }) => {
+  const all = good + neutral + bad;
+  const average = (good * 1 + neutral * 0 + bad * -1) / all;
+  const positive = (good / all) * 100;
+
   return (
     <div>
       <h1>statistic</h1>
       <p>good {good}</p>
       <p>neutral {neutral}</p>
       <p>bad {bad}</p>
+      <p>all {all}</p>
+      <p>average {average}</p>
+      <p>positive {positive}%</p>
     </div>
   );
 };
