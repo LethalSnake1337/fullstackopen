3cb148f2f8714f28ac2146a18965130733a43a2a
LethalSnake1337 <LethalSnake@outlook.com>
2024-09-15 07:41:39 +0200

    Exercise 2.6

---
+++ part02/phonebook/src/App.jsx
@@ -0,0 +1,41 @@
+import { useState } from "react";
+
+const App = () => {
+  const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
+  const [newName, setNewName] = useState("");
+
+  const addPhoneNumber = (event) => {
+    event.preventDefault();
+
+    const newPersonObject = {
+      name: newName,
+    };
+
+    setPersons(persons.concat(newPersonObject));
+    setNewName("");
+  };
+
+  const handlePhoneChange = (event) => {
+    setNewName(event.target.value);
+  };
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={addPhoneNumber}>
+        <div>
+          name: <input value={newName} onChange={handlePhoneChange} />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      {persons.map((person) => (
+        <p key={person.name}>{person.name}</p>
+      ))}
+    </div>
+  );
+};
+
+export default App;
